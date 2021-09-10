ac16a48fe4156a685a08f45792674a8cfba63edf
Darius <darius.mcfarland@blackrock.com>
2021-09-10 02:26:20 -0400

    Moved the home page to its own package

--- gp_mobile/lib/main.dart
+++ gp_mobile/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gp_mobile/pages/home/page_selector.dart';
+import 'package:gp_mobile/pages/home/home.dart';
 // import 'package:firebase_core/firebase_core.dart';
 
 void main() {
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gideon' 's Promise Mobile App',
+      title: 'Gideon\'s Promise Mobile App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -27,46 +27,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Home'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-        automaticallyImplyLeading: true,
-      ),
-      body: PageSelector(),
-// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
