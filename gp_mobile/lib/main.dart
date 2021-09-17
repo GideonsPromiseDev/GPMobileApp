e9f881084e13e93bbb6d40099ef3d1549434fc7e
Darius <darius.mcfarland@blackrock.com>
2021-09-16 23:50:19 -0400

    Added the provider package to the app so that already logged in users can skip the initial sign in page. Created a new widget that would switch between displaying the onboarding page and the rest of the app based on user log in status.

--- gp_mobile/lib/main.dart
+++ gp_mobile/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:gp_mobile/helper_widgets/initial_page_selector.dart';
+import 'package:gp_mobile/models/simple_user.dart';
 import 'package:gp_mobile/themes/gideons_promise_colors.dart';
-
-import 'pages/onboarding/onboarding.dart';
-
-const kBackgroundColor = Color(0xFF202020);
-const kPrimaryColor = Color(0xFFFFBD73);
+import 'package:gp_mobile/utils/firebase_authenticator.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,29 +17,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-// <<<<<<< HEAD
-//       theme: ThemeData(
-//         brightness: Brightness.dark,
-//         primaryColor: kPrimaryColor,
-//         scaffoldBackgroundColor: kBackgroundColor,
-//       ),
-//       home: OnBoardingPage(),
-//       // home: Profile(),
-
-// =======
-      /**
+    return StreamProvider<SimpleUser?>.value(
+      value: FirebaseAuthenticator().simpleUser,
+      initialData: null,
+      child: MaterialApp(
+        /**
        * Sets the app bar and icon colors globally to the specified theme color
        * unless they are specifically overridden within a widget
        */
-      theme: ThemeData.light().copyWith(
-          appBarTheme: AppBarTheme(color: GideonsPromiseColors.blue),
-          iconTheme: IconThemeData(color: GideonsPromiseColors.blue)),
-      darkTheme: ThemeData.dark(),
+        theme: ThemeData.light().copyWith(
+            appBarTheme: AppBarTheme(color: GideonsPromiseColors.blue),
+            iconTheme: IconThemeData(color: GideonsPromiseColors.blue)),
+        darkTheme: ThemeData.dark(),
 
-      // Switches between light and dark theme based on device system theme
-      themeMode: ThemeMode.system,
-      home: OnBoardingPage(),
+        // Switches between light and dark theme based on device system theme
+        themeMode: ThemeMode.system,
+        home: const InitialPageSelector(),
+      ),
     );
   }
 }
