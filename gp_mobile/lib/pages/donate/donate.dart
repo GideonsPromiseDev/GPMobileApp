c7c30e547f0a517460e55e0bc7d2922e08a4f2f6
Mohammed Mehdi <mohammed.mehdi@blackrock.com>
2021-09-16 22:38:38 -0400

    Adding donate files

---
+++ gp_mobile/lib/pages/donate/donate.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gp_mobile/pages/connect_with_us/constants.dart';
+import 'package:gp_mobile/utils/url_launcher.dart';
+
+class Donate extends StatelessWidget {
+  Donate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Donate"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 60, bottom: 40),
+            child: const Image(
+              image: AssetImage("assets/GP_Logo.png"),
+              height: 300,
+              width: 300,
+              fit: BoxFit.contain,
+            ),
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {
+                  openURL(class DonateConstants.donateURL);
+                },
+                icon: const FaIcon(FontAwesomeIcons.money),
+                iconSize: 36,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
